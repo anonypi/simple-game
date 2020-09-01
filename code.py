95d87dcb3cc828a61b02f4b07e5733ca089efde9
Rishabh Singhal <54445199+anonypi@users.noreply.github.com>
2020-09-01 22:30:31 +0530

    update

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
 import random
 
+fact_mem = []
+mult_mem = []
+range_mem = 0
+
 def calc_factors(num):
     prime_nos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
     if num == 1:
@@ -8,37 +12,61 @@ def calc_factors(num):
         print("This is a prime number")
     else:
         fact_li = []
-        i=1
-        while i<=num:
-            if num%i == 0:
+        i=2
+        while i<num:
+            if num%i == 0 and not(i in fact_mem):
                 fact_li.append(i)
             i += 1
         random.shuffle(fact_li)
-        print("Following number is one factor of that number (range: 1, number): ", fact_li[0])
+        fact_mem.append(fact_li[0])
+        print("One factor of that number is (Hint: excluding itself): ", fact_li[0])
 
 def calc_multiples(num):
-    multiple = num*random.randint(1,10)
-    print("Following number is a multiple (between [1-10]x) to that number: ",multiple)
+    multiple = num*random.randint(2,10)
+    while multiple in mult_mem:
+        multiple = num*random.randint(2,10)
+    print("One multiple to that number is (Hint: between [2x] to [10x]): ",multiple)
+    mult_mem.append(multiple)
 
 def calc_range(num):
     num1 = 0
     num2 = 0
-    while not((num >= num1 and num <= num2) or (num >= num2 and num <= num1)):
-        num1 = random.randint(1,100)
-        num2 = random.randint(1,100)
-        if (num >= num1 and num <= num2):
-            print("The number lies between: ", num1, ", ", num2)
-        elif (num >= num2 and num <= num1):
-            print("The number lies between: ", num2, ", ", num1)
+    if num>20-4*range_mem and num<80+4*range_mem:
+        while not((num >= num1 and num <= num2) or (num >= num2 and num <= num1)):
+            num1 = random.randint(num-10+2*range_mem,num+10-2*range_mem)
+            num2 = random.randint(num-10+2*range_mem,num+10-2*range_mem)
+            if (num >= num1 and num <= num2):
+                print("The number lies between (inclusive): ", num1, ", ", num2)
+            elif (num >= num2 and num <= num1):
+                print("The number lies between: (inclusive)", num2, ", ", num1)
+    elif num<=20-4*range_mem:
+        while not((num >= num1 and num <= num2) or (num >= num2 and num <= num1)):
+            num1 = random.randint(0,20-4*range_mem)
+            num2 = random.randint(0,20-4*range_mem)
+            if (num >= num1 and num <= num2):
+                print("The number lies between (inclusive): ", num1, ", ", num2)
+            elif (num >= num2 and num <= num1):
+                print("The number lies between: (inclusive)", num2, ", ", num1)
+    elif num>=80+4*range_mem:
+        while not((num >= num1 and num <= num2) or (num >= num2 and num <= num1)):
+            num1 = random.randint(80+4*range_mem,100)
+            num2 = random.randint(80+4*range_mem,100)
+            if (num >= num1 and num <= num2):
+                print("The number lies between (inclusive): ", num1, ", ", num2)
+            elif (num >= num2 and num <= num1):
+                print("The number lies between: (inclusive)", num2, ", ", num1)
 
 
-def clue(num): #operation/clue selector
-    case = random.randint(1,3)
-    if case == 1:
+
+case = list(range(0,3))
+random.shuffle(case)
+
+def clue(num,itr): #operation/clue selector
+    if case[0] == itr%3:
         calc_range(num)
-    elif case == 2:
+    elif case[1] == itr%3:
         calc_multiples(num)
-    elif case == 3:
+    elif case[2] == itr%3:
         calc_factors(num)
 
 
@@ -54,16 +82,18 @@ num = random.randint(1,100)
 points = 100
 deduction = 10
 user_input = 0
+itr = 0;
 
 #menu
 while points>0:
-    clue(num)
+    clue(num,itr)
+    itr += 1
     user_input = int(input("Guess the number: "))
     if user_input == num:
         print("\nNow you got it right!\n")
         break
     else:
-        print("\nYou were wrong this time\n")
+        print("\nYou were very wrong this time!\n")
         points -= deduction
 
 print("Number is: ", num)
